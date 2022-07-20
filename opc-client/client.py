1bf5e67920eca549cf9214ca8746f77bd68f8171
Vinícius Melo <vinimelo@riseup.net>
2022-07-20 00:44:58 -0300

    Removes LevelHandler and csv writer function, comments debug opc variables lines for now

--- opc-client/client.py
+++ opc-client/client.py
@@ -6,21 +6,6 @@ from datetime import datetime
 sys.path.insert(0, "..")
 
 
-def write_to_csv(level_value):
-    dt = datetime.now()
-    ts = datetime.timestamp(dt)
-    with open('level.csv', 'a', newline='') as level_file:
-        fieldnames = ['datetime', 'value']
-        writer = csv.DictWriter(level_file, fieldnames=fieldnames)
-        writer.writerow({'datetime': ts, 'value': round(level_value, 1)})
-
-
-class LevelHandler(object):
-    def datachange_notification(self, node, value, data):
-        # write_to_csv(value)
-        print(f"New value: {round(value, 1)}")
-
-
 async def read_opc_variables():
     client = Client("opc.tcp://localhost:4840")
     async with client:
@@ -31,13 +16,13 @@ async def read_opc_variables():
             ["0:Objects", "2:PyOPCObject", "2:Py_Temperature"])
         py_level = await root.get_child(["0:Objects", "2:PyOPCObject", "2:Py_Level"])
         py_alarm = await root.get_child(["0:Objects", "2:PyOPCObject", "2:Py_Alarm"])
-        print("My object is: ", obj)
-        print("Py_Temperature is: ", py_temperature)
-        print("Py_Level is: ", py_level)
-        print("Py_Alarm is: ", py_alarm)
-        level_handler = LevelHandler()
-        level_sub = await client.create_subscription(500, level_handler)
-        level_handle = await level_sub.subscribe_data_change(py_level)
+        # print("My object is: ", obj)
+        # print("Py_Temperature is: ", py_temperature)
+        # print("Py_Level is: ", py_level)
+        # print("Py_Alarm is: ", py_alarm)
+        # level_handler = LevelHandler()
+        # level_sub = await client.create_subscription(500, level_handler)
+        # level_handle = await level_sub.subscribe_data_change(py_level)
 
         level_value = await client.get_node("ns=2;i=3").get_value()
 
