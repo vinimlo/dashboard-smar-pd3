ce32b3da6c29922d0fbeb56f72a23c681d3c1f64
Vinícius Melo <vinimelo@riseup.net>
2022-07-20 22:07:27 -0300

    Adds other variables in OPC UA mock server

--- opc-client/server.py
+++ opc-client/server.py
@@ -9,28 +9,31 @@ sys.path.insert(0, "..")
 if __name__ == "__main__":
     server = Server()
     server.set_endpoint("opc.tcp://localhost:4840")
-    uri = "vinimlo"
+    uri = "projetos-automacao"
     idx = server.register_namespace(uri)
     objects = server.get_objects_node()
-    myobj = objects.add_object(idx, "PyOPCObject")
-    py_temperature = myobj.add_variable(idx, "Py_Temperature", 0)
-    py_level = myobj.add_variable(idx, "Py_Level", 15.00)
-    py_alarm = myobj.add_variable(idx, "Py_Alarm", True)
-    py_temperature.set_writable()
-    py_level.set_writable()
+    opc_obj = objects.add_object(idx, "PyOPCObject")
+    # py_temperature = opc_obj.add_variable(idx, "Py_Temperature", 0)
+    py_level = opc_obj.add_variable(idx, "Py_Level", 5.00)
+    py_temperature = opc_obj.add_variable(idx, "Py_Temperature", 5.00)
+    py_flow = opc_obj.add_variable(idx, "Py_Flow", 5.00)
+    py_alarm = opc_obj.add_variable(idx, "Py_Alarm", True)
+    # py_temperature.set_writable()
+    # py_level.set_writable()
 
     server.start()
 
     try:
         count = 5.0
         while True:
-            time.sleep(1)
+            time.sleep(2)
             if(count < 99.5):
                 py_level.set_value(count)
+                py_temperature.set_value(count/2)
+                py_flow.set_value(count/4)
                 count += 0.5
             else:
                 count = 5.0
-            py_temperature.set_value(26.72)
     finally:
         print("Stopping")
         server.stop()
