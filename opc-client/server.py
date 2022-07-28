a8f007826f986b3ea290a67a098d44e8dc603cee
Vinícius Melo <vinimelo@riseup.net>
2022-07-28 15:04:56 -0300

    Faz decaimento de valor de temperatura do tanque 2

--- opc-client/server.py
+++ opc-client/server.py
@@ -39,23 +39,26 @@ if __name__ == "__main__":
             _server.py_level_1.set_value(
                 0) if py_level_1_value < 0 else _server.py_level_1.set_value(py_level_1_value)
 
-            if py_level_1_value > 80:
-                drain_water(_server)
-                _server.py_temperature_2.set_value(py_temperature_1_value)
-            else:
-                _server.py_flow_2.set_value(0.0)
-
             _server.py_flow_1.set_value(
                 0) if py_flow_1_value < 0 else _server.py_flow_1.set_value(py_flow_1_value)
 
             _server.py_temperature_1.set_value(
                 py_temperature_1_value - 0.0006) if py_temperature_1_value > 25 else _server.py_temperature_1.set_value(py_temperature_1_value)
 
+            _server.py_temperature_2.set_value(
+                py_temperature_2_value - 0.0006) if py_temperature_2_value > 25 else _server.py_temperature_2.set_value(py_temperature_2_value)
+
             _server.py_alarm_high_temperature_1.set_value(
                 True) if py_temperature_1_value > 50 else _server.py_alarm_high_temperature_1.set_value(False)
 
             _server.py_alarm_high_temperature_2.set_value(
                 True) if py_temperature_2_value > 50 else _server.py_alarm_high_temperature_2.set_value(False)
+
+            if py_level_1_value > 80:
+                drain_water(_server)
+                _server.py_temperature_2.set_value(py_temperature_1_value)
+            else:
+                _server.py_flow_2.set_value(0.0)
             time.sleep(0.1)
     finally:
         print("Stopping")
