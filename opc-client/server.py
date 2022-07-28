a90130858b917b9aa4d3d77df73176d35d6d285d
Vinícius Melo <vinimelo@riseup.net>
2022-07-28 11:19:21 -0300

    Refatora server para ser mais enxuto, remove TKinter

--- opc-client/server.py
+++ opc-client/server.py
@@ -1,58 +1,34 @@
-from dotenv import load_dotenv
-from tkinter import Label, Frame, Tk, PhotoImage, ttk
-
-from opc_server import OpcServer
-
-import os
 import sys
+import os
 import time
 
+from dotenv import load_dotenv
+from opc_server import OpcServer
+
 load_dotenv()
+
+_server = OpcServer(os.getenv("OPC_SERVER_URI"))
+
 sys.path.insert(0, "..")
 
-opc_server = OpcServer(os.getenv("OPC_SERVER_URI"))
-
-tk = Tk()
-main_frame = Frame(tk)
-main_frame.pack()
-# test = PhotoImage(file="./test.png")
-# btn = Label(main_frame, image=test)
-# btn.bind(on_click)
-
-
-def increase_level_value():
-    if(opc_server.py_bomb_1.get_value() and (opc_server.py_level < 99)):
-        opc_server.increase_value(2)
-        time.sleep(1)
-        increase_level_value()
-    opc_server.py_bomb_1.set_value(False)
-
-def activate_bomb_1():
-    opc_server.py_bomb_1.set_value(True)
-    increase_level_value()    
-
-
-ttk.Button(main_frame, text="Acionar bomba 1",
-           command=activate_bomb_1).pack(pady=10)
-
 if __name__ == "__main__":
-    opc_server.configure_server()
-    opc_server.server.start()
+    _server.server.start()
+    _server.configure_server()
 
     try:
-        tk.mainloop()
-    #     count = 5.0
-    #     while True:
-    #         time.sleep(2)
-    #         if(count < 99.5):
-    #             py_level_1.set_value(count)
-    #             py_temperature_1.set_value(count/2)
-    #             py_flow_1.set_value(count/4)
-    #             py_temperature_2.set_value(50 - count/2)
-    #             py_flow_2.set_value(50 - count/4)
-    #             count += 0.5
-    #         else:
-    #             count = 5.0
+        while True:
+            py_level_1_value = _server.py_level_1.get_value()
+            py_temperature_1_value = _server.py_temperature_1.get_value()
+            py_flow_1_value = _server.py_flow_1.get_value()
+
+            _server.py_alarm_low_level_1.set_value(True) if py_level_1_value < 50 else _server.py_alarm_low_level_1.set_value(False)
+
+            _server.py_level_1.set_value(0) if py_level_1_value < 0 else _server.py_level_1.set_value(py_level_1_value)
+
+            _server.py_flow_1.set_value(0) if py_flow_1_value < 0 else _server.py_flow_1.set_value(py_flow_1_value)
+
+            _server.py_temperature_1.set_value(py_temperature_1_value-0.0006) if py_temperature_1_value > 0 else _server.py_temperature_1.set_value(py_temperature_1_value)
+            time.sleep(0.1)
     finally:
         print("Stopping")
-        opc_server.server.stop()
+        _server.server.stop()
