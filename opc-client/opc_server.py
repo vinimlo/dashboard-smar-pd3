385bd3647d2cc7dcd40faa749e2eae99885dd436
Vinícius Melo <vinimelo@riseup.net>
2022-07-28 11:20:19 -0300

    Ajusta as variáveis cadastradas no servidor para as novas necessidades

--- opc-client/opc_server.py
+++ opc-client/opc_server.py
@@ -12,28 +12,32 @@ class OpcServer:
         self.opc_obj = self.__objects.add_object(self.__idx, "Py_PD3_Objects")
 
         # Add tank mock variables
-        self.__py_level = self.add_read_only_variable("Py_Level_1", 5.00)
-        self.__py_temperature_1 = self.add_read_only_variable(
-            "Py_Temperature_1", 5.00)
-        self.__py_flow_1 = self.add_read_only_variable("Py_Flow_1", 5.00)
-        self.__py_temperature_2 = self.add_read_only_variable(
-            "Py_Temperature_2", 5.00)
-        self.__py_flow_2 = self.add_read_only_variable("Py_Flow_2", 5.00)
+        self.py_level_1 = self.add_writable_variable("Py_Level_1", 0.00)
+        self.py_temperature_1 = self.add_writable_variable(
+            "Py_Temperature_1", 15.00)
+        self.py_flow_1 = self.add_writable_variable("Py_Flow_1", 0.00)
+        self.py_temperature_2 = self.add_writable_variable(
+            "Py_Temperature_2", 10.00)
+        self.py_flow_2 = self.add_writable_variable("Py_Flow_2", 0.00)
 
         # Add tank mock alarms
-        self.__py_alarm_panel = self.add_read_only_variable(
+        self.py_alarm_panel = self.add_writable_variable(
             "Py_Alarm_Panel", False)
-        self.__py_alarm_emergency = self.add_read_only_variable(
+        self.py_alarm_emergency = self.add_writable_variable(
             "Py_Alarm_Emergency", False)
-        self.__py_alarm_low_level_1 = self.add_read_only_variable(
+        self.py_alarm_low_level_1 = self.add_writable_variable(
             "Py_Alarm_Low_Level_1", False)
-        self.__py_alarm_high_temperature_1 = self.add_read_only_variable(
+        self.py_alarm_high_temperature_1 = self.add_writable_variable(
             "Py_Alarm_High_Temperature_1", False)
-        self.__py_alarm_high_temperature_2 = self.add_read_only_variable(
+        self.py_alarm_high_temperature_2 = self.add_writable_variable(
             "Py_Alarm_High_Temperature_2", False)
 
         self.py_bomb_1 = self.add_writable_variable(
             "Py_Bomb_1", False)
+        self.py_valve_1 = self.add_writable_variable(
+            "Py_Valve_1", False)
+        self.py_heater = self.add_writable_variable(
+            "Py_Heater", False)
 
     def add_read_only_variable(self, variable_name, value):
         variable = self.opc_obj.add_variable(
@@ -50,13 +54,3 @@ class OpcServer:
     @property
     def server(self):
         return self.__server
-
-    @property
-    def py_level(self):
-        return self.__py_level.get_value()
-
-    def increase_value(self, value_to_increase):
-        value = self.__py_level.get_value()
-        value += value_to_increase
-        self.__py_level.set_value(value)
-        return value
